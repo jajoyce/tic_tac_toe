b9f03d291119e490136bad6cfac1742fd810480f
jajoyce <jjoyce4@gmail.com>
2021-12-28 17:42:58 -0500

    Commit 2 - Add message element in JS DOM, announce player turns, check if clicked square already filled

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,35 @@
 let squares = document.querySelectorAll('.square');
 let restart = document.querySelector('.restart');
+let message = document.getElementById('message');
 
 let turn = 1;
 
-function addXO(event) {
-    if (turn % 2 === 0) {
-        event.target.innerHTML = 'O';
-    } else {
-        event.target.innerHTML = 'X';
-    }
-    console.log('Called');
-    turn++;
-}
-
 for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', addXO);
+}
+
+restart.addEventListener('click', clearBoard);
+
+function addXO(event) {
+    if (event.target.innerHTML) {
+        message.innerHTML = 'Choose an empty square!';
+    } else if (turn % 2 === 0) {
+        event.target.innerHTML = 'O';
+        turn++;
+        message.innerHTML = 'Player X, your turn!';
+    } else {
+        event.target.innerHTML = 'X';
+        turn++;
+        message.innerHTML = 'Player O, your turn!';
+    }
+    console.log('Called');
 }
 
 function clearBoard() {
     for (let i = 0; i < squares.length; i++) {
         squares[i].innerHTML = '';
     }
+    message.innerHTML = 'Player X goes first, Player O second.'
 }
 
-restart.addEventListener('click', clearBoard);
 
